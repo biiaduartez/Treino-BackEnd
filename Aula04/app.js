4b2f017afed7921b55fdb63e4091cac088c30e24
BIANCA FERREIRA DUARTE <22282117@CFP127ALUNO.LOCAL>
2023-02-10 07:51:52 -0300

    aula04 concluída

--- Aula04/app.js
+++ Aula04/app.js
@@ -19,46 +19,51 @@ var entradaDados = readline.createInterface({
     output: process.stdout
 });
 
-// Entrada de dados
+//Entrada de dados valor1
 entradaDados.question('Valor1: \n', function(numero1) {
+
+    //replace - permite trocar um conteudo da string por outro 
     let valor1 = numero1.replace(',', '.');
 
+    //Entrada de dados valor2
     entradaDados.question('Valor2: \n', function(numero2) {
-        let valor2 = numero2.replace(',', '.');
 
-        entradaDados.question('Escolha uma operação: { SOMAR | SUBTRAIR | MULTIPLICAR | DIVIDIR }: \n', function(tipoCalculo) {
+        let valor2 = numero2;
+        //Entrada de dados Tipo de Calculo
+        entradaDados.question('Escolha uma operação: [Somar | Subtrair | Multiplicar | Dividir]: \n', function(tipoCalculo) {
 
-            // toUpperCase = ABCDE (Converte a string para MAIUSCULO )
-            // toLowerCase = abcde (Converte a string para minusculo )
-
+            //toUpperCase - Converte uma string para MAIUSCULO
+            //toLowerCase - Converte uma string para minusculo 
             let operacao = tipoCalculo.toUpperCase();
             let resultado;
 
-            // Validação de entrada de dados
-            if (valor1 == '' || valor2 == '' || operacao == '') {
-                console.log('ERRO: Não é possível calcular sem preencher todos os dados')
+            //Validação de entrada de dados vazios 
+            if (valor1 == "" || valor2 == "" || operacao == "") {
+                console.log('Erro: Não é possível calcular sem preencher todos os dados.');
                 entradaDados.close();
-
-                /*Validação para verificar se os dados digitados são números
-                 - Podemos utilizar isNan ou o typeof
-                 - Só que se usarmos o tyoeof é preciso garantir que o tipo
-                de dados não é genérico (string)*/
-
-                //   } else if (typeof(valor1) != 'number' || (valor2) != 'number') {
+                //Validação para verificar se os dados digitados são números
+                //Podemos utilizar (isNaN ou typeof)
+                //Se usar o typeof, precisa garantir que o tipo de dados não é generico (string)
+                //else if (typeof(valor1) != 'number' || typeof(valor2) != 'number') {
 
             } else if (isNaN(valor1) || isNaN(valor2)) {
-                console.log('EERO: Não é possível calcular sem a entrada de valores numéricos');
-                entradaDados.close();
-
+                console.log('Erro: Não é possível calcular sem a entrada de valores númericos')
+                entradaDados.close(); //Fecha o objeto de entrada de dados (encerra o programa)
             } else {
 
+                //chama a função calcular, encaminhando os dados para o calculo
                 resultado = matematica.calcular(valor1, valor2, operacao);
-                console.log(resultado);
 
+                //valida se o retorno da função é verdadeiro ou falso(se for falso, encerra o programa)
+
+                // if (resultado == false && typeof(resultado) == 'boolean'){   ->  1 metodo - Tratativa para saber se o 0 é numerico ou booleano 
+                // if (resultado === false) {                                   ->  2 metodo - Fazendo a comparação do conteudo e tipagem com os 3 iguais                                 
+                if (resultado === false) {
+                    entradaDados.close();
+                } else {
+                    console.log(resultado);
+                }
             }
-
         })
-
-    });
-
-});
+    })
+})
--- Aula04/modulo/calculadora.js
+++ Aula04/modulo/calculadora.js
@@ -15,39 +15,87 @@ function calcular(numero1, numero2, tipoCalculo) {
     let operacao = tipoCalculo.toUpperCase();
 
     let resultado;
+    let status = true; //variavel boleana para identificar o status do calculo
 
 
-    if (operacao == 'SOMAR')
-        resultado = (valor1) + (valor2)
 
-    else if (operacao == 'SUBTRAIR')
-        resultado = (valor1) - (valor2)
+    // if (operacao == 'SOMAR')
+    //     resultado = (valor1) + (valor2)
 
-    else if (operacao == 'MULTIPLICAR')
-        resultado = (valor1) * (valor2)
+    // else if (operacao == 'SUBTRAIR')
+    //     resultado = (valor1) - (valor2)
 
-    else if (operacao == 'DIVIDIR') {
-        if (valor2 == 0) { // Validação do divisor 0
-            console.log('ERRO: Não é possível fazer uma uma divisão por 0')
-            entradaDados.close();
-        } else
-            resultado = (valor1) / (valor2)
+    // else if (operacao == 'MULTIPLICAR')
+    //     resultado = (valor1) * (valor2)
 
-    } else {
-        console.log('ERRO: a informação não é uma operação válida')
-        entradaDados.close(); // fecha o objeto de entrada de dados
+    // else if (operacao == 'DIVIDIR') {
+    //     if (valor2 == 0) { // Validação do divisor 0
+    //         console.log('ERRO: Não é possível fazer uma uma divisão por 0')
+    //         entradaDados.close();
+    //     } else
+    //         resultado = (valor1) / (valor2)
+
+    // } else {
+    //     console.log('ERRO: a informação não é uma operação válida')
+    //     entradaDados.close(); // fecha o objeto de entrada de dados
+    // }
+
+    switch (operacao) {
+        case 'SOMAR':
+            // resultado = somar(valor1,valor2); -> trabalhando com o modelo arrow function 
+            resultado = (valor1) + (valor2);
+            break;
+
+        case 'SUBTRAIR':
+            // resultado = subtrair(valor1,valor2); -> trabalhando com o modelo arrow function
+            resultado = (valor1) - (valor2);
+            break;
+
+        case 'MULTIPLICAR':
+            // resultado = multiplicar(valor1,valor2); -> trabalhando com o modelo arrow function
+            resultado = (valor1) * (valor2);
+            break;
+
+        case 'DIVIDIR':
+            //validação da divisão por 0 
+            if (valor2 == 0) {
+                console.log('Erro: Não é possível fazer uma divisão por 0.')
+                    //entradaDados.close();
+                status = false;
+            } else {
+                // resultado = dividir(valor1,valor2); -> trabalhando com o modelo arrow function
+                resultado = Number(valor1) / Number(valor2);
+            }
+            break;
+            //Similar ao else final de uma estrutura baseada em if/else (se nenhuma das opções for verdadeira, sempre passará pelo default)        
+        default:
+            console.log('Erro: A operação informada não é valida. Confira a sua entrada.')
+            status = false;
     }
 
-    //validação para tratar oa variável quando nenhum calculo é realizado
-    if (resultado != undefined)
+    //Validação para tratar a variavel resultado quando nenhum calculo é realizado
+    if (resultado != undefined) {
         return resultado;
-    else
-        return false;
-
-
+    } else {
+        return status;
+    }
 }
 
-// exporta uma função ara ser utilizada em outro arquivo
+// Forma 02 de se criar uma função em JS (terá o mesmo conteúdo da função anterior)
+// const calcular = function(numero1, numero2, tipoCalculo) {
+
+// }
+
+//Todas essas funçoes serão calculos privados
+// Forma 03 de se criar uma função em JS (modelo arrow function)
+//dispensa o uso da palavra function, e do uso do return
+const somar = (valor1, valor2) => Number(valor1) + Number(valor2);
+const subtrair = (valor1, valor2) => Number(valor1) - Number(valor2);
+const multiplicar = (valor1, valor2) => Number(valor1) * Number(valor2);
+const dividir = (valor1, valor2) => Number(valor1) / Number(valor2);
+
+
+//Exporta uma função para ser utilizada em outro arquivo
 module.exports = {
     calcular
 }
